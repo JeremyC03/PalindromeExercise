2c10dc5fe7c09761d1b3aa615ac20208c9177824
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-07-14 12:23:09 -0400

    Finished Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] array = word.ToCharArray();
+            Array.Reverse(array);
+            string palindrome = new string(array);
+
+            if (word == palindrome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]// ← you will need to put some test data and expected result here.
+        [InlineData("noon", true)]
+        [InlineData("level", true)]
+        [InlineData("radar", true)]
+        [InlineData("civil", false)]
+        public void PalindromeTest(string word, bool expected)
         {
-
+            //Arrange
+            WordSmith instance = new WordSmith();
+            //Act
+            bool actual = instance.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
